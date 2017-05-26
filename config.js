ab0fcf6179ea1727a43e503c5dc8040fa16eb151
zueore <zueore@gmail.com>
2017-05-26 19:09:07 +0530

    Production

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var twitterauth = {
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.token_secret
+  consumer_key: process.env.consumer_key.toString(),
+  consumer_secret: process.env.consumer_secret.toString(),
+  access_token: process.env.access_token.toString(),
+  access_token_secret: process.env.token_secret.toString()
 }
 module.exports = {twitterauth};
