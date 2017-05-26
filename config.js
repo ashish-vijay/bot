cccab4e2666fc6ad5ab57b5f3def32e56e25fca4
zueore <zueore@gmail.com>
2017-05-26 19:13:12 +0530

    Production

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
+var consumer_key = process.env.consumer_key;
+var consumer_secret = process.env.consumer_secret;
+var access_token = process.env.access_token;
+var access_token_secret = process.env.token_secret;
+
 var twitterauth = {
-  consumer_key: process.env.consumer_key.toString(),
-  consumer_secret: process.env.consumer_secret.toString(),
-  access_token: process.env.access_token.toString(),
-  access_token_secret: process.env.token_secret.toString()
+  consumer_key: consumer_key.toString(),
+  consumer_secret: consumer_secret.toString(),
+  access_token: access_token.toString(),
+  access_token_secret: access_token_secret.toString()
 }
 module.exports = {twitterauth};
