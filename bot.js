72075d21f216ca21c5f42b2cdbc6f0c42604e398
zueore <zueore@gmail.com>
2017-05-27 19:35:21 +0530

    Added reply function

--- bot.js
+++ bot.js
@@ -3,12 +3,13 @@ const twit = require('twit');
 const {twitterauth} = require('./config.js');
 
 var app = express();
-var twitter = new twit(twitterauth);
 var port = process.env.PORT || 3000;
+var twitter = new twit(twitterauth);
+var stream = twitter.stream('user');
 
 var retweet = function() {
   var params = {
-    q: '#node.js'
+    q: '#haveabook OR #og OR #owlgeneration'
   };
   twitter.get('/search/tweets', params, function(err, data) {
     if(!err){
@@ -34,8 +35,32 @@ var retweet = function() {
   });
 };
 
+stream.on('follow', followed);
+
+function followed(event) {
+    console.log('Follow Event is running');
+    var
+      name = event.source.name,
+      screenName = event.source.screen_name;
+    tweetNow('@' + screenName + '\“So many books, so little time.\”');
+};
+
+function tweetNow(tweetText) {
+    var tweet = {
+        status: tweetText
+    }
+    twitter.post('statuses/update', tweet, function(err, data) {
+      if(err){
+        console.log("Error in Follow Tweet");
+      }
+      else{
+        console.log("Follow Tweet Working Successfully");
+      }
+    });
+}
+
 retweet();
-setInterval(retweet, 5000);
+setInterval(retweet, 300000);
 
 app.listen(port, function() {
   console.log(`Server is up on port ${port}`);
